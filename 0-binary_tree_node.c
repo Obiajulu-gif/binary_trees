f8aae01903a3629a3b16039237732fbabb98b71e
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-02-29 03:39:55 +0100

    task 0 done

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,20 +8,20 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-    /* allocate memory for the new node  */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        return NULL;
-    }
+/* allocate memory for the new node  */
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
 
-    /* initialize new node */
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
+/* initialize new node */
+new_node->parent = parent;
+new_node->n = value;
+new_node->left = NULL;
+new_node->right = NULL;
 
-    return (new_node);
+return (new_node);
 }
