c6f1588149272f8022a901f4c96d016cb3491b43
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-03-01 03:43:24 +0100

    done

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,51 +1,63 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first: A pointer to the first node
- * @second: A pointer to the second node
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: a pointer to the first node to find the ancestor
+ * @second: a pointer to the second node to find the ancestor
  *
- * Return: A pointer to the lowest common ancestor node, or NULL if no common
- *         ancestor was found
+ * Return: pointer to the ancestor node
+ *         NULL if there is no ancestor node
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second)
+				     const binary_tree_t *second)
 {
-const binary_tree_t *temp1, *temp2;
+	size_t depth_first, depth_second;
 
-/* Edge cases */
-if (first == NULL || second == NULL)
-return (NULL);
+	if (!first || !second)
+		return (NULL);
 
-/* Initialize two pointers */
-temp1 = first;
-temp2 = second;
+	depth_first = binary_tree_depth(first);
+	depth_second = binary_tree_depth(second);
 
-/* Traverse upwards from first node */
-while (temp1 != NULL)
-{
-/* Check if second node is found */
-if (temp1 == second || temp1->parent == second)
-return ((binary_tree_t *)temp1);
-
-/* Reset second pointer for each iteration of first node */
-temp2 = second;
-
-/* Traverse upwards from second node */
-while (temp2 != NULL)
-{
-/* Check if first node is found */
-if (temp2 == first || temp2->parent == first)
-return ((binary_tree_t *)temp2);
-
-/* Move second node pointer upwards */
-temp2 = temp2->parent;
+	while (depth_first > depth_second)
+	{
+		first = first->parent;
+		depth_first--;
+	}
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+	return ((binary_tree_t *)first);
 }
 
-/* Move first node pointer upwards */
-temp1 = temp1->parent;
-}
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: node to calculate the depth of
+ *
+ * Return: depth of the node
+ *         0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
 
-/* If no common ancestor found */
-return (NULL);
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
 }
